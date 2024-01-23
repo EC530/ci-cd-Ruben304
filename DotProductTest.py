785a833fe345221e21894de34ba83f5eeea0f6cc
Ruben304 <ruben.carbajal6502@gmail.com>
2024-01-23 10:12:55 -0500

    first push

---
+++ DotProductTest.py
@@ -0,0 +1,10 @@
+# This matrix is to 
+def DotProductTest(MatrixA, MatrixB):
+    if MatrixA or MatrixB == 0:
+        return 0
+    
+    # matrix and a scalar
+    # mismatched matrix sizes
+    # characters in number
+    # input validation to organize matrix inputs
+    return 0
